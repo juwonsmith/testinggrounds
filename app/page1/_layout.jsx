abe33c9a2e294ee53874dcdbc3abe93ec03a2b7e
juwonoladele <juwonoladele772@gmail.com>
2023-09-30 19:17:00 +0100

    v1

---
+++ app/page1/_layout.jsx
@@ -0,0 +1,32 @@
+"use client";
+import Link from "next/link";
+import { useEffect } from "react";
+export default function page1Layout({
+  children, // will be a page or nested layout
+}) {
+  useEffect(() => {
+    console.log("layout mounted");
+  }, []);
+  return (
+    <section
+      className="w-full h-32 items-center justify-between 
+      p-4 flex list-none first-letter first-letter
+       bg-gradient-to-tl from-cyan-500 to-gray-950 bg-slate-600 "
+    >
+      <div
+        className=" gap-6 items-center justify-center h-16 cursor-pointer
+       font-nunito text-white font-bold  flex"
+      >
+        <Link href="/page1/inner">
+          <li className="navlink">innerworld</li>
+        </Link>
+
+        <Link href="../page1">
+          <li className="navlink">page1</li>
+        </Link>
+      </div>
+
+      {children}
+    </section>
+  );
+}
---
+++ app/page1/inner/page.jsx
@@ -0,0 +1,3 @@
+export default function page() {
+  return <div>page1 inner</div>;
+}
