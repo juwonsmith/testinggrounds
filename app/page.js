1f55fc172d2e400c64c74ac80d157132badc140e
juwonoladele <juwonoladele772@gmail.com>
2023-09-30 19:40:21 +0100

    updated link

--- app/page.js
+++ app/page.js
@@ -1,3 +1,13 @@
+import Link from "next/link";
 export default function Home() {
-  return <main className=""></main>;
+  return (
+    <main className="flex  justify-center">
+      <Link
+        href="/page1"
+        className="text-center flex justify-center bg-gray-600 rounded w-[10rem] h-max"
+      >
+        page 1 link
+      </Link>
+    </main>
+  );
 }
